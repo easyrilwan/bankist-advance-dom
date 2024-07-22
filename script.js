c896c283a6b40cc8c0315c78772e72ca75977b30
easyrilwan <easyrilwan@gmail.com>
2024-07-23 00:11:34 +0100

    Event Delegation

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 "use strict";
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function () {
   modal.classList.remove("hidden");
@@ -30,10 +32,22 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
+///////////////////////////////////////
+// Button Scrolling
 
 btnScrollTo.addEventListener("click", function (e) {
   // Scrolling Modern Way
   section1.scrollIntoView({ behavior: "smooth" });
+});
+
+///////////////////////////////////////
+// Page Navigation
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching Strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
